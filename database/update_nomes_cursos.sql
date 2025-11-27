b78c0cd11eb610943502dbbf16bd180f9703d6c1
Lucas Simões <lucasdascsimoes@gmail.com>
2025-11-27 18:01:15 -0300

    fix(database): padroniza nomenclatura de cursos para siglas

---
+++ database/update_nomes_cursos.sql
@@ -0,0 +1,49 @@
+UPDATE `eleicao` SET curso = 'DSM'
+WHERE curso IN (
+    'Desenvolvimento de Software Multiplataforma',
+    'desenvolvimento de software multiplataforma',
+    'DSM - Desenvolvimento de Software Multiplataforma',
+    'Desenv. Software Multiplataforma'
+);
+
+UPDATE `eleicao` SET curso = 'GE'
+WHERE curso IN (
+    'Gestão Empresarial',
+    'Gestao Empresarial',
+    'gestao empresarial',
+    'GESTAO EMPRESARIAL'
+);
+
+UPDATE `eleicao` SET curso = 'GPI'
+WHERE curso IN (
+    'Gestão da Produção Industrial',
+    'Gestao da Producao Industrial',
+    'gestao da producao industrial',
+    'GESTAO DA PRODUCAO INDUSTRIAL',
+    'Gestao Producao Industrial'
+);
+
+UPDATE `aluno` SET curso = 'DSM'
+WHERE curso IN (
+    'Desenvolvimento de Software Multiplataforma',
+    'desenvolvimento de software multiplataforma',
+    'DSM - Desenvolvimento de Software Multiplataforma',
+    'Desenv. Software Multiplataforma'
+);
+
+UPDATE `aluno` SET curso = 'GE'
+WHERE curso IN (
+    'Gestão Empresarial',
+    'Gestao Empresarial',
+    'gestao empresarial',
+    'GESTAO EMPRESARIAL'
+);
+
+UPDATE `aluno` SET curso = 'GPI'
+WHERE curso IN (
+    'Gestão da Produção Industrial',
+    'Gestao da Producao Industrial',
+    'gestao da producao industrial',
+    'GESTAO DA PRODUCAO INDUSTRIAL',
+    'Gestao Producao Industrial'
+);
