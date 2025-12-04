3baada464b1faf242ce86626a9f4f07c937a5257
Lucas Simões <lucasdascsimoes@gmail.com>
2025-12-04 17:27:57 -0300

    feat(user): esconde link de inscrição após candidatura cadastrada

--- public/pages/user/components/header.php
+++ public/pages/user/components/header.php
@@ -6,6 +6,31 @@ if (!isset($eleicaoCandidatura)) {
     $semestre = $usuario['semestre'];
     $eleicaoCandidatura = buscarEleicaoAtivaComVerificacao($curso, $semestre, 'candidatura');
 }
+
+// Verificar se o aluno já tem candidatura cadastrada
+if (!isset($tem_candidatura)) {
+    $usuario = obterUsuarioLogado();
+    $id_aluno = $usuario['id'];
+    $curso = $usuario['curso'];
+    $semestre = $usuario['semestre'];
+
+    $tem_candidatura = false;
+
+    if ($eleicaoCandidatura) {
+        $stmt = $conn->prepare("
+            SELECT c.id_candidatura
+            FROM CANDIDATURA c
+            JOIN ELEICAO e ON c.id_eleicao = e.id_eleicao
+            WHERE c.id_aluno = ?
+            AND e.curso = ?
+            AND e.semestre = ?
+            ORDER BY c.data_inscricao DESC
+            LIMIT 1
+        ");
+        $stmt->execute([$id_aluno, $curso, $semestre]);
+        $tem_candidatura = ($stmt->fetch() !== false);
+    }
+}
 ?>
 <header class="site">
     <nav class="navbar">
@@ -16,7 +41,7 @@ if (!isset($eleicaoCandidatura)) {
 
         <ul class="links">
             <li><a href="../../pages/user/index.php" <?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'class="active"' : '' ?>>Home</a></li>
-            <?php if ($eleicaoCandidatura): ?>
+            <?php if ($eleicaoCandidatura && !$tem_candidatura): ?>
                 <li><a href="../../pages/user/inscricao.php" <?= basename($_SERVER['PHP_SELF']) == 'inscricao.php' ? 'class="active"' : '' ?>>Inscrição</a></li>
             <?php endif; ?>
             <li><a href="../../pages/user/votacao.php" <?= basename($_SERVER['PHP_SELF']) == 'votacao.php' ? 'class="active"' : '' ?>>Votação</a></li>
@@ -32,8 +57,8 @@ if (!isset($eleicaoCandidatura)) {
                         <span>Mudar Foto de Perfil</span>
                     </a>
                     <a href="../../pages/user/editar_perfil.php">
-                        <i class="fas fa-camera"></i>
-                        <span>Mudar Informações de Usuário</span>
+                        <i class="fas fa-user-edit"></i>
+                        <span>Editar Perfil</span>
                     </a>
                 </div>
             </div>
